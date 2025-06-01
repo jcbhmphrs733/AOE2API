5460db5a2a1e18324628f82c46fae9c1f1f2cf53
Beduman <behunz77@gmail.com>
2025-05-31 23:53:59 -0700

    building fix

--- controllers/buildingController.js
+++ controllers/buildingController.js
@@ -2,68 +2,68 @@ const mongodb = require('../db/mongodb');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-    const result = await mongodb.getDatabase().collection('civs').find();
+    const result = await mongodb.getDatabase().collection('buildings').find();
     result.toArray()
-        .then((civs) => {
+        .then((buildings) => {
             res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(civs);
+            res.status(200).json(buildings);
         })
         .catch(err => {
-            res.status(500).json({ error: 'Could not fetch civ' });
+            res.status(500).json({ error: 'Could not fetch building' });
         });
 }
 
 const getSingle = async (req, res) => {
-    const civId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().collection('civs').find({ _id: civId });
+    const buildingId = new ObjectId(req.params.id);
+    const result = await mongodb.getDatabase().collection('buildings').find({ _id: buildingId });
     result.toArray()
-        .then((civs) => {
+        .then((buildings) => {
             res.setHeader('Content-Type', 'application/json');
-            res.status(200).json(civs[0]);
+            res.status(200).json(buildings[0]);
         })
         .catch(err => {
-            res.status(500).json({ error: 'Could not fetch civ' });
+            res.status(500).json({ error: 'Could not fetch building' });
         });
 }
 
 const postBuilding = async (req, res) => {;
-    const newCiv = {
+    const newBuilding = {
         name: req.body.name,
         baseHp: req.body.baseHp,
         cost: req.body.cost
     };
 
-    const response = await mongodb.getDatabase().collection('civs').insertOne(newCiv);
+    const response = await mongodb.getDatabase().collection('buildings').insertOne(newBuilding);
     if (response.acknowledged) {
         res.status(204).send();
     } else {
-        res.status(500).json({ error: 'Could not create civ' });
+        res.status(500).json({ error: 'Could not create Building' });
     }
 }
 
 const putBuilding = async (req, res) => {
-    const civId = new ObjectId(req.params.id);
-    const updatedCiv = {
+    const buildingId = new ObjectId(req.params.id);
+    const updatedBuilding = {
         name: req.body.name,
         baseHp: req.body.baseHp,
         cost: req.body.cost
     };
 
-    const response = await mongodb.getDatabase().collection('civs').replaceOne({ _id: civId }, updatedCiv);
+    const response = await mongodb.getDatabase().collection('buildings').replaceOne({ _id: buildingId }, updatedBuilding);
     if (response.modifiedCount > 0) {
-        res.status(204).json({ message: 'civ updated successfully' });
+        res.status(204).json({ message: 'Building updated successfully' });
     } else {
-        res.status(500).json({ error: 'Could not update civ' });
+        res.status(500).json({ error: 'Could not update Building' });
     }
 }
 
 const deleteBuilding = async (req, res) => {
-    const civId = new ObjectId(req.params.id); 
-    const response = await mongodb.getDatabase().collection('civs').deleteOne({ _id: civId });
+    const buildingId = new ObjectId(req.params.id); 
+    const response = await mongodb.getDatabase().collection('buildings').deleteOne({ _id: buildingId });
     if (response.deletedCount > 0) {
-        res.status(204).json({ message: 'Civ deleted successfully' });
+        res.status(204).json({ message: 'Building deleted successfully' });
     } else {
-        res.status(500).json({ error: 'Could not delete Civ' });
+        res.status(500).json({ error: 'Could not delete Building' });
     }
 }
 
